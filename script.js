71724d90ed7379b92840592f598a06462bbc4e1b
leahtorres <leah_torres@davincischools.org>
2017-11-29 21:27:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function myFunction(id) {
     document.getElementById("demo").classList.toggle("disappear");
 }
+
+
+function comic1(elem) {
+	elem.classList.toggle("comic11");
+}
+function comic1(elem){
+	elem.classList.toggle("comic111");
+}
+function comic2(elem) {
+	elem.classList.toggle("comic22");
+}
+function comic2(elem){
+	elem.classList.toggle("comic222");
+}
