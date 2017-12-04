cb052c8eef86880b463810f999e515b8b6b4c2bc
leahtorres <leah_torres@davincischools.org>
2017-12-03 22:26:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function comicc(elem) {
 function comicd(elem) {
 	elem.classList.toggle("comic222");
 }
+function comice(elem) {
+	elem.classList.toggle("comic66");
+}
+function comicf(elem) {
+	elem.classList.toggle("comic55");
+}
+function comicg(elem) {
+	elem.classList.toggle("comic77");
+}
+function comich(elem) {
+	elem.classList.toggle("comic88");
+}
