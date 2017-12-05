e4014afe0642439505431881b620017ab2eb0d93
leahtorres <leah_torres@davincischools.org>
2017-12-05 14:45:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//In html I made a button, but then when it's clicked the id named demo with a class of disappear, will display none in css, the paragragh that appears when the button is clicked will disappear when you click again.
+//this effect happens with the use of toggle, and the reson I'm using document.getelementbyid, I want to indicate that specific id found in html, to get the effect.
 function myFunction(id) {
     document.getElementById("demo").classList.toggle("disappear");
 }
+//the following lines from 7 to 30 are all similar but for different divs. Elem is used rather than document piece of code because the div is a class.
+//the on.. the word in the percentisis, in css with a dot infront adding a content of a image so it changes to another image.
 function ona(elem) {
 elem.classList.toggle("ona");
 }
